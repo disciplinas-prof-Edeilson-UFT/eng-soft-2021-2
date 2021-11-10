c39f9b8cc67c622680662f61a2f6a8885c7775f9
Arthur <arthurffreis@gmail.com>
2021-11-10 19:29:24 -0300

    alterando a classe 2

--- lib/components/busca/banner_de_categoria.dart
+++ lib/components/busca/banner_de_categoria.dart
@@ -1,110 +1,57 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ifood_engenharia_de_software/utilities/cores.dart';
 
-class BannerDeCategorias extends StatelessWidget {
-  final String categoria;
 
-  const BannerDeCategorias(
-    {Key? key, required this.categoria}) 
-    : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style =
-    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold), fixedSize: const Size(195, 70), primary: AppCores.vermelhoPrincipal );
-    
-    return Center(
-     child: Column(
-        children: <Widget>[
-          const SizedBox(height: 10),
-          ElevatedButton(
-            style: style,
-            onPressed: () {},
-            child: Column(
-              
-              children: [
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    Text(categoria)
-                  ],
-                )
-              ]
-            )
-          )
-        ]  
-      ),
-    );
- }
+class Categoria {
+  final String nome;
+  const Categoria(this.nome);
 }
 
 class BannersDeCategorias extends StatelessWidget {
- 
+  
+  final List <Categoria>listacategoria;
+
   const BannersDeCategorias(
-    {Key? key,})
+    {Key? key,required this.listacategoria})
     : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return Center(
-      
-        child: Row(
-          children:[
-            const SizedBox(width: 2),
-            Column(
-              children: const [
-                BannerDeCategorias(
-                  categoria:'Express',
-                ),
-                BannerDeCategorias(
-                  categoria:'Farmácia',
-                ),
-                BannerDeCategorias(
-                  categoria:'Brasileira',
-                ),
-                BannerDeCategorias(
-                 categoria:'Marmita',
-                ),
-                BannerDeCategorias(
-                  categoria:'Carnes',
-                ),
-                BannerDeCategorias(
-                  categoria:'Contemporânea',
-                ),
-                BannerDeCategorias(
-                  categoria:'Japonesa',
-                ) 
-              ],
-            ),
-            const SizedBox(width: 17),
-            Column(
-              children: const [
-                BannerDeCategorias(
-                  categoria:'Mercado',
-                ),
-                BannerDeCategorias(
-                  categoria:'Bebidas',
-                ),
-                BannerDeCategorias(
-                  categoria:'Saudável',
-                ),
-                BannerDeCategorias(
-                  categoria:'Lanches',
-                ),
-                BannerDeCategorias(
-                  categoria:'Italianos',
-                ),
-                BannerDeCategorias(
-                  categoria:'Pizza',
-                ),
-                BannerDeCategorias(
-                  categoria:'Poke',
-                )
+    
+    final ButtonStyle style =
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold), fixedSize: const Size(195, 70), primary: AppCores.vermelhoPrincipal );
+    
+    
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: listacategoria.length,
+      itemBuilder:(BuildContext context, int index){
+          return Column(
+            children:[
+              const SizedBox(width: 4),
+              Column(
+                children:  [
+                  ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 15),
+                        Row(
+                         children: [
+                          Text(listacategoria[index].nome)
+                         ],
+                        )
+                      ]
+                    )
+                  )
                 ]
-              )
-            ]  
-          )
+              ),   
+            ]
+         );
+      }
     );
   }
 }
--- lib/pages/busca.dart
+++ lib/pages/busca.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ifood_engenharia_de_software/components/busca/banner_de_categoria.dart';
+import 'package:ifood_engenharia_de_software/utilities/cores.dart';
+
 class PaginaBusca extends StatefulWidget {
   const PaginaBusca({Key? key}) : super(key: key);
 
@@ -13,14 +15,29 @@ class _PaginaBuscaState extends State<PaginaBusca>
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Column(
-        children: const[ 
-          Text(
-            'Busca',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        children: [ 
+          Container(
+            width: double.infinity,
+            height: 160,
+            color: AppCores.cinza1,
+            child: Wrap(alignment: WrapAlignment.center, children: const [
+              Text(
+                'Pedidos',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize:25),
+              ),
+            ])
           ),
-          BannersDeCategorias()
-        ]
-      ),
+          const Align(
+           alignment: Alignment.topLeft,
+           child: BannersDeCategorias(listacategoria: [Categoria('lista1')]), /*coluna esquerda*/
+          ),
+          const Align(
+            alignment: Alignment.topRight,
+            
+            child:BannersDeCategorias(listacategoria: [Categoria('lista2')]) /*coluna direita*/
+          )
+        ] 
+      )    
     );
   }
-}
+ }     
