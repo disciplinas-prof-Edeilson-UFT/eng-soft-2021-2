223f298b9330e3288df526353505983e4f96d44d
Lindomar Rodrigues <lindomarrodriguesdeazevedo@outlook.com>
2021-11-10 08:45:07 -0300

    Definindo a lista de categorias

--- lib/components/inicio/card_da_categoria_componente.dart
+++ lib/components/inicio/card_da_categoria_componente.dart
@@ -3,39 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:ifood_engenharia_de_software/utilities/cores.dart';
 
 class CategoriaCard extends StatelessWidget {
+  // get itens => List <String>.generate(15,(i)=>"item $i");
+  final List categoriasLista = ['Mercado', 'Pizza', 'Promoções'];
 
-  get itens => List <String>.generate(15,(i)=>"item $i");
-
-  const CategoriaCard({Key? key}) : super(key: key);
+  CategoriaCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
         height: 200,
         child: ListView.builder(
-
             scrollDirection: Axis.horizontal,
             physics: const BouncingScrollPhysics(), // this for snapping
-            itemCount: 15,
+            itemCount: categoriasLista.length,
             itemBuilder: (_, index) => SizedBox(
-              width: MediaQuery.of(context).size.width * 0.3,
-              child: Column(children: [
-                const SizedBox(height: 40),
-                Container(
-                  height: 40,
-                  width: 90,
-                  padding: const EdgeInsets.all(50),
-                  decoration: const BoxDecoration(
-                      color: AppCores.vermelhoPrincipal,
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                ),
-                const SizedBox(height: 10),
-
-                 Text(
-                  '${itens[index]}',
-                  style: TextStyle(color: Color(0xff717171), fontSize: 14),
-                )
-              ]),
-            )));
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  child: Column(children: [
+                    const SizedBox(height: 40),
+                    Container(
+                      height: 40,
+                      width: 90,
+                      padding: const EdgeInsets.all(50),
+                      decoration: const BoxDecoration(
+                          color: AppCores.vermelhoPrincipal,
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      '${categoriasLista[index]}',
+                      style: const TextStyle(color: Color(0xff717171), fontSize: 14),
+                    )
+                  ]),
+                )));
   }
 }
