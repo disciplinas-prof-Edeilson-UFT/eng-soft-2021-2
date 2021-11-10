31d2d91abccea08bfa4e54ca5ec2bb5c1e1607fa
Mateus-Arauj <mateus9509595091@gmail.com>
2021-11-10 20:57:59 -0300

    Ultimas lojas-update2

---
+++ lib/components/inicio/ultimas_lojas_componente.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ifood_engenharia_de_software/utilities/cores.dart';
+
+class UltimasLojasRestaurante extends StatelessWidget {
+  final List<String> listalojas;
+  const UltimasLojasRestaurante({Key? key, required this.listalojas})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      const Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Últimas lojas',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            )),
+      Expanded(
+          child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: listalojas.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Column(
+                    children: [
+                      const CircleAvatar(
+                        radius: 33,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          listalojas[index],
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              color: AppCores.preto54,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }))
+    ]);
+  }
+}
--- lib/pages/inicio.dart
+++ lib/pages/inicio.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ifood_engenharia_de_software/components/restaurantes/ultimas_lojas_componente.dart';
+import 'package:ifood_engenharia_de_software/components/inicio/ultimas_lojas_componente.dart';
 
 class PaginaInicio extends StatefulWidget {
   const PaginaInicio({Key? key}) : super(key: key);
@@ -18,16 +18,7 @@ class _PaginaInicioState extends State<PaginaInicio>
         'Início',
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
       ),
-      Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: EdgeInsets.only(left: 15.0),
-            child: Text(
-              'Últimas lojas',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-          )),
-      Expanded(child: UltimasLojasRestaurante())
+      Expanded(child: UltimasLojasRestaurante(listalojas: ['Loja exemplo1', 'Loja exemplo2', 'Loja exemplo3', 'Loja exemplo4'],))
     ]));
   }
 }
