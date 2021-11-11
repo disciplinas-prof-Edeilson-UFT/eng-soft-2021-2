7450540723f879592b4f5cda547092cb8757fd67
Rafael-jooj <71270235+Rafael-jooj@users.noreply.github.com>
2021-11-11 00:50:37 -0300

    R3/T2 - Lista de opções parte inferior do perfil - Rafael

--- lib/components/perfil/abas_inferiores_do_perfil.dart
+++ lib/components/perfil/abas_inferiores_do_perfil.dart
@@ -8,30 +8,15 @@ class AbaInfCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFFFFFF),
       body: Column(
         children: [
-          Container(
-            width: double.infinity,
-            child: Wrap(alignment: WrapAlignment.center, children: const [
-              Text(
-                'Perfil',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-              ),
-              SizedBox(height: 60),
-            ]),
-          ),
-          Container(
-            height: 270,
-          ),
-
           Container(
             width: 370,
             alignment: Alignment.bottomLeft,
             child: Row(
               children: const <Widget>[
                 Icon(Icons.help_outline, color: Colors.black38,),
-                Text(' Ajuda', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black45),),
+                Text(' Ajuda', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 19, color: Colors.black45),),
                 Spacer(),
                 Icon(Icons.arrow_forward_ios_sharp, color: Colors.black38,),
               ],
@@ -39,7 +24,7 @@ class AbaInfCard extends StatelessWidget {
             ),
             decoration: const BoxDecoration(
                 border: Border(
-                  bottom: BorderSide(color: Colors.black26, width: 2,),
+                  bottom: BorderSide(color: Colors.black26, width: 1,),
                 )
             ),
             padding: const EdgeInsets.only(bottom: 20, top: 20),
@@ -51,7 +36,7 @@ class AbaInfCard extends StatelessWidget {
             child: Row(
               children: const <Widget>[
                 Icon(Icons.settings, color: Colors.black38,),
-                Text(' Configurações', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black45),),
+                Text(' Configurações', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 19, color: Colors.black45),),
                 Spacer(),
                 Icon(Icons.arrow_forward_ios_sharp, color: Colors.black38,),
               ],
@@ -59,7 +44,7 @@ class AbaInfCard extends StatelessWidget {
             ),
             decoration: const BoxDecoration(
                 border: Border(
-                  bottom: BorderSide(color: Colors.black26, width: 2,),
+                  bottom: BorderSide(color: Colors.black26, width: 1,),
                 )
             ),
             padding: const EdgeInsets.only(bottom: 20, top: 20),
@@ -71,7 +56,7 @@ class AbaInfCard extends StatelessWidget {
             child: Row(
               children: const <Widget>[
                 Icon(Icons.security, color: Colors.black38,),
-                Text(' Segurança', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black45),),
+                Text(' Segurança', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 19, color: Colors.black45),),
                 Spacer(),
                 Icon(Icons.arrow_forward_ios_sharp, color: Colors.black38,),
               ],
@@ -79,7 +64,7 @@ class AbaInfCard extends StatelessWidget {
             ),
             decoration: const BoxDecoration(
                 border: Border(
-                  bottom: BorderSide(color: Colors.black26, width: 2,),
+                  bottom: BorderSide(color: Colors.black12, width: 1,),
                 )
             ),
             padding: const EdgeInsets.only(bottom: 20, top: 20),
@@ -91,7 +76,7 @@ class AbaInfCard extends StatelessWidget {
             child: Row(
               children: const <Widget>[
                 Icon(Icons.qr_code, color: Colors.black38,),
-                Text(' Usar no carro', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black45),),
+                Text(' Usar no carro', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 19, color: Colors.black45),),
                 Spacer(),
                 Icon(Icons.arrow_forward_ios_sharp, color: Colors.black38,),
               ],
@@ -99,7 +84,7 @@ class AbaInfCard extends StatelessWidget {
             ),
             decoration: const BoxDecoration(
                 border: Border(
-                  bottom: BorderSide(color: Colors.black26, width: 2,),
+                  bottom: BorderSide(color: Colors.black12, width: 1,),
                 )
             ),
             padding: const EdgeInsets.only(bottom: 20, top: 20),
@@ -111,7 +96,7 @@ class AbaInfCard extends StatelessWidget {
             child: Row(
               children: const <Widget>[
                 Icon(Icons.add_business_outlined, color: Colors.black38,),
-                Text(' Sugerir restaurantes', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black45),),
+                Text(' Sugerir restaurantes', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 19, color: Colors.black45),),
                 Spacer(),
                 Icon(Icons.arrow_forward_ios_sharp, color: Colors.black38,),
               ],
@@ -119,7 +104,7 @@ class AbaInfCard extends StatelessWidget {
             ),
             decoration: const BoxDecoration(
                 border: Border(
-                  bottom: BorderSide(color: Colors.black26, width: 2,),
+                  bottom: BorderSide(color: Colors.black26, width: 1,),
                 )
             ),
             padding: const EdgeInsets.only(bottom: 20, top: 20),
@@ -130,6 +115,8 @@ class AbaInfCard extends StatelessWidget {
     );
   }
 }
+
+
 
 
 
--- lib/pages/perfil.dart
+++ lib/pages/perfil.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ifood_engenharia_de_software/components/perfil/abas_inferiores_do_perfil.dart';
-import 'package:ifood_engenharia_de_software/utilities/cores.dart';
 
 class PaginaPerfil extends StatefulWidget {
   const PaginaPerfil({Key? key}) : super(key: key);
@@ -10,19 +9,34 @@ class PaginaPerfil extends StatefulWidget {
   _PaginaPerfilState createState() => _PaginaPerfilState();
 }
 
-class _PaginaPerfilState extends State<PaginaPerfil> {
+class _PaginaPerfilState extends State<PaginaPerfil>
+    with SingleTickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
+          Container(
+            child: Wrap(alignment: WrapAlignment.center, children: const [
+              Text(
+                'Perfil',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ]),
+          ),
           const Expanded (child: AbaInfCard())
         ],
       ),
+      //body: Column(
+
+        //children: const [
+        //],
+      //),
     );
 
   }
 }
+
 
 
 
