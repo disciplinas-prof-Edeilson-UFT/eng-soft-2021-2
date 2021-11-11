efa24ac75c212df90639b8484f39ba946d0de79a
Lindomar Rodrigues <lindomarrodriguesdeazevedo@outlook.com>
2021-11-11 09:06:42 -0300

    Resolvendo dependencias

--- lib/pages/inicio.dart
+++ lib/pages/inicio.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ifood_engenharia_de_software/components/inicio/card_da_categoria_componente.dart';
 import 'package:ifood_engenharia_de_software/components/inicio/menu_de_redirecionamento.dart';
+import 'package:ifood_engenharia_de_software/components/inicio/ultimas_lojas_componente.dart';
 import 'package:ifood_engenharia_de_software/utilities/cores.dart';
 
 class PaginaInicio extends StatefulWidget {
@@ -19,13 +20,14 @@ class _PaginaInicioState extends State<PaginaInicio>
         Text('inicio',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
         Menu(),
-          CategoriaCard(categoriaLista: [
-            Categoria('Pizza', AppCores.vermelhoPrincipal),
-            Categoria('Mercado', Color(0xffb5cf47)),
-            Categoria('Vale Alimentacao', Color(0xffA553BE)),
-            Categoria('Sorvete', Color(0xffF8879B)),
-            Categoria('Hamburguer', Color(0xffA553BE)),
-            Categoria('Doces', Color(0xffF8879B))]),
+        CategoriaCard(categoriaLista: [
+          Categoria('Pizza', AppCores.vermelhoPrincipal),
+          Categoria('Mercado', Color(0xffb5cf47)),
+          Categoria('Vale Alimentacao', Color(0xffA553BE)),
+          Categoria('Sorvete', Color(0xffF8879B)),
+          Categoria('Hamburguer', Color(0xffA553BE)),
+          Categoria('Doces', Color(0xffF8879B))
+        ]),
         Expanded(
             child: UltimasLojasRestaurante(
           listalojas: [
