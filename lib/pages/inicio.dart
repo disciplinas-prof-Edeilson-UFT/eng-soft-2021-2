e153a96193361b515e0a29485cadce7263635860
Lindomar Rodrigues <lindomarrodriguesdeazevedo@outlook.com>
2021-11-11 08:48:37 -0300

    Resolvendo conflitos

--- lib/pages/inicio.dart
+++ lib/pages/inicio.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ifood_engenharia_de_software/components/inicio/card_da_categoria_componente.dart';
+import 'package:ifood_engenharia_de_software/components/inicio/ultimas_lojas_componente.dart';
+import 'package:ifood_engenharia_de_software/utilities/cores.dart';
 
 class PaginaInicio extends StatefulWidget {
   const PaginaInicio({Key? key}) : super(key: key);
@@ -12,29 +15,27 @@ class _PaginaInicioState extends State<PaginaInicio>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Text('inicio',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-          CategoriaCard(categoriaLista: [
-            Categoria('Pizza', AppCores.vermelhoPrincipal),
-            Categoria('Mercado', Color(0xffb5cf47)),
-            Categoria('Vale Alimentacao', Color(0xffA553BE)),
-            Categoria('Sorvete', Color(0xffF8879B)),
-            Categoria('Hamburguer', Color(0xffA553BE)),
-            Categoria('Doces', Color(0xffF8879B)),
-          ])
-        ],
-      ),
-      Expanded(
-          child: UltimasLojasRestaurante(
-        listalojas: [
-          'Loja exemplo1',
-          'Loja exemplo2',
-          'Loja exemplo3',
-          'Loja exemplo4'
-        ],
-      ))
-    ]));
+      body: Column(children: const [
+        Text('inicio',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+        CategoriaCard(categoriaLista: [
+          Categoria('Pizza', AppCores.vermelhoPrincipal),
+          Categoria('Mercado', Color(0xffb5cf47)),
+          Categoria('Vale Alimentacao', Color(0xffA553BE)),
+          Categoria('Sorvete', Color(0xffF8879B)),
+          Categoria('Hamburguer', Color(0xffA553BE)),
+          Categoria('Doces', Color(0xffF8879B))
+        ]),
+        Expanded(
+            child: UltimasLojasRestaurante(
+          listalojas: [
+            'Loja exemplo1',
+            'Loja exemplo2',
+            'Loja exemplo3',
+            'Loja exemplo4'
+          ],
+        ))
+      ]),
+    );
   }
 }
