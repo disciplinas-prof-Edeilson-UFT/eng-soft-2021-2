368be0d9cea94f668a6f7ed733753c6413eb9796
Lindomar Rodrigues <lindomarrodriguesdeazevedo@outlook.com>
2021-11-15 18:21:51 -0300

    Adicionando estado necessário para o card de "Peça novamente"

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,5 +1,12 @@
+import 'models/peca_novamente_model.dart';
+
 class AppState {
   final int paginaAtual;
+  final PecaNovamenteModel pecaNovamenteData;
 
-  AppState({this.paginaAtual = 0});
+  AppState(
+      {this.paginaAtual = 0,
+      this.pecaNovamenteData = const PecaNovamenteModel(
+          'https://aguardando.fase.de.utilizar.o.storage/',
+          [Produto('Pizza G Calabresa C/Borda Mista', 1)])});
 }
