a455893c5905d4e533877c613cd8f50b06e2b252
Vinicius <viniciusoliveira926@gmail.com>
2021-12-01 17:32:21 -0300

    config padaro

--- lib/components/perfil/cabecalho_perfil_componente.dart
+++ lib/components/perfil/cabecalho_perfil_componente.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:ifood_engenharia_de_software/utilities/cores.dart';
+import 'package:ifood_engenharia_de_software/models/cabecalho_perfil_model.dart';
 
 class TopoPagina extends StatefulWidget {
   final User user;
@@ -38,43 +41,51 @@ class _TopoPaginaState extends State<TopoPagina>
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 35, bottom: 45, right: 20),
-            child: Image.network(
-              "https://imgur.com/EPUWxPP.png",
-              height: 50,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 45, right: 20, bottom: 30),
-            child: Text(
-              '${_currentUser.displayName}',
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.topRight,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.qr_code_scanner_outlined,
-                  size: 25,
-                ),
-                color: AppCores.vermelhoPrincipal,
-                onPressed: readQRCode
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 35, bottom: 45, right: 20),
+              child: Image.network(
+                "https://imgur.com/EPUWxPP.png",
+                height: 50,
               ),
             ),
-          ),
-        ],
-      )
-    );
+            Padding(
+              padding: const EdgeInsets.only(top: 45, right: 20, bottom: 30),
+              child: Text(
+                '${_currentUser.displayName}',
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    icon: const Icon(
+                      Icons.qr_code_scanner_outlined,
+                      size: 25,
+                    ),
+                    color: AppCores.vermelhoPrincipal,
+                    onPressed: readQRCode),
+              ),
+            ),
+          ],
+        ));
   }
 }
+
+/**
+Padding(
+              padding: const EdgeInsets.only(top: 35, bottom: 45, right: 20),
+              child: Image.network(
+                "https://imgur.com/EPUWxPP.png",
+                height: 50,
+              ),
+            ),
+
+*/
