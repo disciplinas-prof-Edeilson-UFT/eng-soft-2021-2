d9843c3172fb7b1e2f4e675b21c850fd72dd6c76
Wanderson Bueno <wanolibueno@gmail.com>
2021-12-01 20:58:27 -0300

    resolvendo conflito de rebase

--- lib/models/card_convide_model.dart
+++ lib/models/card_convide_model.dart
@@ -1,4 +1,5 @@
 class CardCovideAmigos {
+<<<<<<< HEAD
   final String img_url;
   final String codigo_convide;
 
@@ -7,6 +8,17 @@ class CardCovideAmigos {
   factory CardCovideAmigos.fromFirestore(firestoreData) {
     // TODO
     return CardCovideAmigos(codigo_convide: '', img_url: '');
+=======
+  final String imageConvideAmigos;
+  final String codigoConvideAmigos;
+
+  CardCovideAmigos(
+      {required this.imageConvideAmigos, required this.codigoConvideAmigos});
+
+  factory CardCovideAmigos.fromFirestore(firestoreData) {
+    // TODO
+    return CardCovideAmigos(codigoConvideAmigos: '', imageConvideAmigos: '');
+>>>>>>> 7ee480e (page covide amigos e model)
   }
 
   toFirestore() {
