af672b396c36bdadeb17a6b35d1483ce66e470d8
Rafael-jooj <71270235+Rafael-jooj@users.noreply.github.com>
2021-12-02 15:57:20 -0300

    resolvendo conflitos

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,24 +1,27 @@
-import 'models/mais_pedidos_por_vc_model.dart';
-import 'models/peca_novamente_model.dart';
-import 'models/banner_de_categoria_model.dart';
-import 'models/card_da_categoria_componente_model.dart';
-import 'models/abas_inferiores_model.dart';
 import 'package:flutter/material.dart';
 import 'package:ifood_engenharia_de_software/utilities/cores.dart';
+import 'models/ultimas_lojas_model.dart';
+
+
+import 'models/banner_de_categoria_model.dart';
+import 'models/card_da_categoria_componente_model.dart';
+import 'models/mais_pedidos_por_vc_model.dart';
+import 'models/peca_novamente_model.dart';
+import 'models/abas_inferiores_model.dart';
 
 class AppState {
   final int paginaAtual;
   final PecaNovamenteModel pecaNovamenteData;
   final CategoriaModel cardcategoriaData;
   final MaisPedidosModel maispedidosData;
+  final UltimasLojasModel ultimaslojasData;
   final BannerDeCategoriaModel bannerdecategoriaData;
   final AbasInferioresModel abasinferioresData;
 
   AppState(
       {this.paginaAtual = 0,
-      this.pecaNovamenteData = const PecaNovamenteModel(
-          'https://aguardando.fase.de.utilizar.o.storage/',
-          [Produto('Pizza G Calabresa C/Borda Mista', 1)]),
+
+      this.pecaNovamenteData = const PecaNovamenteModel(),
       this.bannerdecategoriaData = const BannerDeCategoriaModel([
         CategoriaBanner('Salgados',
             'https://cdn.discordapp.com/attachments/814528592419094570/912145821808599060/salgados1.jpg'),
@@ -67,6 +70,14 @@ class AppState {
         MaisPedidos('Vikings', '',
             'https://cdn.discordapp.com/attachments/809775517493624852/910736208626323497/22132_800x800.png')
       ]),
+        this.ultimaslojasData = const UltimasLojasModel(
+      [
+      Lojas("McDonald's",'https://i.imgur.com/L8VcJbD.png'),
+      Lojas('Burger King','https://i.imgur.com/BhBX8HH.png'),
+      Lojas('Giraffas','https://i.imgur.com/0I9W33L.png'),
+      Lojas('Subway','https://i.imgur.com/rYyDTK6.png'),
+      ]),
+      
       this.abasinferioresData = const AbasInferioresModel([
         Abas("Ajuda",'https://imgur.com/NgRmdFM.png'),
         Abas("Configurações",'https://imgur.com/TseB9Qd.png'),
