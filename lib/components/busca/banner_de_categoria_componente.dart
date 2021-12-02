762179b2b1912b6e3224263b657cae64165f6a9b
Luana <luana.lorena@mail.uft.edu.br>
2021-12-02 12:29:54 -0300

    R1-T2 Enviando novamente o mecanismo de busca-Luana

--- lib/components/busca/banner_de_categoria_componente.dart
+++ lib/components/busca/banner_de_categoria_componente.dart
@@ -4,15 +4,15 @@ import 'package:ifood_engenharia_de_software/models/banner_de_categoria_model.da
 
 class BannersDeCategorias extends StatelessWidget {
   final List<CategoriaBanner> listacategoria;
-  
+
   const BannersDeCategorias({Key? key, required this.listacategoria})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return GridView.builder(
         scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: (4 / 2),
@@ -22,53 +22,24 @@ class BannersDeCategorias extends StatelessWidget {
         itemCount: listacategoria.length,
         itemBuilder: (BuildContext context, index) {
           return Container(
-            height: 100,
-            width: 400,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.grey[100],
-            image:  DecorationImage(
-              fit: BoxFit.fill,   
-              image: NetworkImage(listacategoria[index].imgurl)
-            ),
-          ),  
-
-            child:Padding(
-                padding: const EdgeInsets.all(5),
-                child:Text(listacategoria[index].nome,
-                  style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),)
-                )
-          );
-        }
-    );
+              height: 100,
+              width: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.grey[100],
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(listacategoria[index].imgurl)),
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Text(
+                    listacategoria[index].nome,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  )));
+        });
   }
 }
-
-              
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
